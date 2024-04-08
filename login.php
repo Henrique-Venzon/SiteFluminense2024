15b9eff910615cb3dc7390fb91916b55e0314784
Henrique Francisco Venzon <143184156+Henrique-Venzon@users.noreply.github.com>
2024-04-07 22:27:04 -0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+
+        <title>ENTRAR e REGISTRAR</title>
+        <link rel="stylesheet" href="estilo.css">
+        <script src="script.js" defer></script>
+
+    </head>
+    <body>
+  
+        <div class="entrarRegistrar">
+
+        <div class="caixaRegistrar">
+            <div>
+            <form  action="" method="post">
+            <div class="formLogin">
+            <h1 class="entrarText">CADASTRO</h1>
+            <label class="labelR" for="nome1">Nome:</label>
+            <input class="labelRinput" type="text" id="nome1" name="nome1"><br>
+            <label class="labelR" for="email1">Email:</label>
+            <input class="labelRinput" type="email" id="email1" name="email1"><br>
+            <label class="labelR" for="senha1">Senha:</label>
+            <input class="labelRinput" type="password" id="senha1" name="senha1"><br>
+            <input class="botaoR" type="submit" value="REGISTRAR">
+            </div>
+            </form>
+            </div>
+        </div>
+        <div class="imagemFlu">
+            <img class="imagemMeio" src="img/fluLogo.png">
+        </div>
+        
+        <div class="caixaEntrar">
+            <div >
+            <form action="" method="post">
+            <div class="formLogin">
+            <h1 class="TextVerde">ENTRAR</h1>
+            <label class="labelV" for="nome2">Nome:</label>
+            <input class="labelVinput" type="text" id="nome2" name="nome"><br>
+            <label class="labelV" for="email2">Email:</label>
+            <input class="labelVinput" type="email" id="email2" name="email"><br>
+            <label class="labelV" for="senha2">Senha:</label>
+            <input class="labelVinput" type="password" id="senha2" name="senha"><br>
+            <input class="botaoV" type="submit" value="ENTRAR">
+            </div>
+            </form>
+            </div>
+        </div>
+        </div>
+  
+    </body>
+</html>
